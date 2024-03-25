e67f7227fc142e808d7f8479b58a2c951e8ec510
paulusminus <info@paulmin.nl>
2024-03-25 19:35:47 +0100

    Moved example to separate crate

---
+++ crates/example/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    env::args,
+    io::{BufRead, BufReader, Read},
+};
+
+use rw::{error_line, LineError};
+use rw::generic::reader::Reader;
+
+fn process_lines<F, R>(f: F) -> impl Fn(R) -> Result<(), LineError>
+where
+    F: Fn(String) + Copy,
+    R: Read,
+{
+    move |r| {
+        BufReader::new(r)
+            .lines()
+            .enumerate()
+            .map(|(no, rs)| rs.map(f).map_err(error_line(Some(no + 1))))
+            .collect::<Result<Vec<_>, _>>()
+            .map(|_| ())
+    }
+}
+
+fn args_argument(prefixes: &[&str]) -> Option<String> {
+    args()
+        .skip(1)
+        .position(|s| prefixes.contains(&(s.as_str())))
+        .and_then(|i| args().skip(1).nth(i + 1))
+}
+
+fn print(s: String) {
+    println!("{}", s);
+}
+
+fn main() -> Result<(), LineError> {
+    match args_argument(&["-f", "--file"]).map(Reader::try_from_file) {
+        Some(file_result) => file_result
+            .map_err(error_line(None))
+            .and_then(process_lines(print)),
+        None => Ok(Reader::default()).and_then(process_lines(print)),
+    }
+}
